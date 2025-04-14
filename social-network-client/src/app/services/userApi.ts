29d7dda34b834bb68ff68f513fdeb1dc90b15f87
kenzha-web <kenzha.web@gmail.com>
2025-04-14 19:14:40 +0500

    add user, posts, comments, likes, follow services

---
+++ social-network-client/src/app/services/userApi.ts
@@ -0,0 +1,60 @@
+import {api} from "./api.ts";
+import {User} from "../types.ts";
+
+export const userApi = api.injectEndpoints({
+	endpoints: (builder) => ({
+		login: builder.mutation<
+			{ token: string },
+			{ email: string; password: string }
+		>({
+			query: (userData) => ({
+				url: '/login',
+				method: 'POST',
+				body: userData
+			})
+		}),
+		register: builder.mutation<
+			{ email: string; password: string; name: string },
+			{ email: string; password: string; name: string }
+		>({
+			query: (userData) => ({
+				url: '/register',
+				method: 'POST',
+				body: userData
+			})
+		}),
+		current: builder.query<User, void>({
+			query: () => ({
+				url: '/current',
+				method: 'GET'
+			})
+		}),
+		getUserById: builder.query<User, void>({
+			query: (id) => ({
+				url: `/users/${id}`,
+				method: 'GET'
+			})
+		}),
+		updateUser: builder.mutation<User, { userData: FormData, id: string }>({
+			query: ({userData, id}) => ({
+				url: `/users/${id}`,
+				method: 'PUT',
+				body: userData
+			})
+		}),
+	})
+})
+
+export const {
+	useRegisterMutation,
+	useLoginMutation,
+	useCurrentQuery,
+	useLazyCurrentQuery,
+	useGetUserByIdQuery,
+	useLazyGetUserByIdQuery,
+	useUpdateUserMutation
+} = userApi;
+
+export const {
+	endpoints: { login, register, current, getUserById, updateUser }
+} = userApi;
