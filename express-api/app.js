bbf7845a170cddb0337a73bcb2dade941b20383b
kenzha-web <kenzha.web@gmail.com>
2025-05-25 20:02:15 +0500

    add docker compose, nginx

--- express-api/app.js
+++ express-api/app.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:3000', 'http://localhost'],
   credentials: true
 }));
 app.use(logger('dev'));
